040872949f0e26de38650e591ccf79d6c2c066ce
cosmosha <cosmoshagan@gmail.com>
2023-06-06 23:46:59 +0000

    Fix linters error

--- student.rb
+++ student.rb
@@ -1,7 +1,7 @@
-require './person.rb'
+require './person'
 
 class Student < Person
-  def initialize(age, classroom, name = 'unknown', parent_permission: true)
+  def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
@@ -9,5 +9,4 @@ class Student < Person
   def play_hooky
     '¯\(ツ)/¯'
   end
-
 end
