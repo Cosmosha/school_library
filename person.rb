628890dd95b6f2fb10e36d236585c4a27ae1a5bf
cosmosha <cosmoshagan@gmail.com>
2023-06-06 23:58:11 +0000

    Fix linters errors

--- person.rb
+++ person.rb
@@ -1,15 +1,16 @@
 class Person
   attr_value = :id, :name, :age
 
-  def initialize(name = 'unknown', age, parent_permission: true)
+  def initialize(age, name = 'unknown', parent_permission: true)
     @id = Random.rand(1..100)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
-    return @parent_permission == true || of_age?
+    return true if @parent_permission == true || of_age?
 
     false
   end
