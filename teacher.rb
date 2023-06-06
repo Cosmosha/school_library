77f1e7c74fe5a82176d77741239224f55ef5e6a6
cosmosha <cosmoshagan@gmail.com>
2023-06-06 23:33:15 +0000

    Fix linters error

--- teacher.rb
+++ teacher.rb
@@ -1,13 +1,12 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-  def initialize(age, name='unknown', specialization, parent_permission: true)
-    super(age, name, parent_permissions: parent_permissions)
+  def initialize(age, specialization, name = 'uknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @specialization = specialization
   end
 
   def can_use_services?
     true
   end
-
 end
