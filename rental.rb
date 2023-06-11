f39b20cb94fe03d1eed7128323047c8d18942111
cosmosha <cosmoshagan@gmail.com>
2023-06-11 04:31:51 +0000

    Update rental file

--- rental.rb
+++ rental.rb
@@ -4,9 +4,7 @@ class Rental
   def initialize(date, person, book)
     @date = date
     @person = person
-    person.add_rental(self)
     @book = book
-    book.add_rental(self)
 
     person.rental << self
     book.rental << self
